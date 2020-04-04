6cab9a6a23448736a859855d5079cdc974645c08
BoySandyUtomo <boysandy1601@gmail.com>
2020-04-04 10:46:38 +0700

    fixed

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,46 @@
 @section('title', 'Admin')
 
 @section('content')
+<form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search" action="{{ url('/search')}}" method="GET">
+            <div class="input-group">
+              <input type="text" name="search" class="form-control bg-light border-0 small" value="" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+              <div class="input-group-append">
+                <button class="btn btn-primary" type="submit">
+                  <i class="fas fa-search fa-sm"></i>
+                </button>
+              </div>
+            </div>
+          </form>
+
+          <!-- Topbar Navbar -->
+          <ul class="navbar-nav ml-auto">
+
+            <!-- Nav Item - User Information -->
+            <li class="nav-item dropdown no-arrow">
+              <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Boy Sandy</span>
+                <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
+              </a>
+              <!-- Dropdown - User Information -->
+              <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href=""
+                   onclick="event.preventDefault();
+                      document.getElementById('logout-form').submit();">
+                      {{ __('Logout') }}
+                    </a>
+                      <form id="logout-form" action="" method="POST" style="display: none;">
+                                        @csrf
+                        </form>
+                
+              </div>
+            </li>
+
+          </ul>
+
+        </nav>
+        <!-- End of Topbar -->
+        
         <!-- Begin Page Content -->
         <div class="container-fluid">
 
--- resources/views/jurusan/index.blade.php
+++ resources/views/jurusan/index.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Jurusan')
 
 @section('content')
- <!-- Topbar Search -->
+ 
  <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search" action="{{ url('/search')}}" method="GET">
             <div class="input-group">
               <input type="text" name="search" class="form-control bg-light border-0 small" value="" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
@@ -73,7 +73,7 @@
           <tbody>
           @foreach($jurusan as $jur)
               <tr>
-                  <td>{{ $jur->id_jur }}</td>
+              <th scope="row">{{ $loop->iteration }}</th>
                   <td>
                   @foreach($fakultas as $fak)
                       @if($fak->id_fak == $jur->id_fak)
